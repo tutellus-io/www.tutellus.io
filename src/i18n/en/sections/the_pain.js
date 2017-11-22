548c6fbbd50f4471b2743866df99df21f06b60f1
Miguel Caballero <mcaballero@tutellus.com>
2017-11-22 15:12:00 +0100

    copies mejorados

--- src/i18n/en/sections/the_pain.js
+++ src/i18n/en/sections/the_pain.js
@@ -1,5 +1,5 @@
 //@flow
 export const the_pain = {
-    title: "<strong>The pain</strong>: studying is hard and it doesn’t help to decrease poverty worldwide",
-    description: "EdTech platforms requiere a hard & expensive process to learn and to improve your knowledge the enough to be able to get a better job. And they don’t let people to earn money studying, so Education really doesn’t let people to decrease their poverty.",
+    title: "<strong>The pain</strong>: studying is hard and it doesn’t help to reduce poverty worldwide",
+    description: "Learning in an EdTech platform is a hard an expensive process if you want to improve your skills enough to get a job. And you can't make money by doing it: people can not get out of poverty by studying.",
 };
