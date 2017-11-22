56993665e01b38ee72a2820d2a09b4d16c5d18d8
Javier Ortiz <jortiz@tutellus.com>
2017-11-23 00:38:52 +0100

    Valida el email, v1

--- src/pages/management/index.js
+++ src/pages/management/index.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import {parse} from 'query-string';
-import {Redirect, Link} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import _ from 'lodash';
 import {PageContent, SectionTitle, Text, ColumnCenter, LinkButton} from '../../components';
 import {translate} from 'react-i18next';
-import styled from 'styled-components';
-import styles from '../../styles';
 
 
 const EmailVerified = ({t}) =>
@@ -46,7 +44,8 @@ export class ManagementElement extends Component {
         this.verifyEmail = this.verifyEmail.bind(this);
     }
 
-    componentDidMount() {
+    componentWillMount() {
+        console.log('componentWillMount');
         const {
             db,
             location: {
@@ -54,47 +53,61 @@ export class ManagementElement extends Component {
             },
         } = this.props;
 
-        const url_params = _.pick(parse(search), ['mode', 'oobCode', 'continueUrl']);
-        const auth = db.auth();
-
-        const views_Fn = {
-            resetPassword: Promise.resolve(this.STATES.RESET_PASSWORD),
-            verifyEmail: this.verifyEmail,
-        };
-
-        if (_.has(views_Fn, url_params.mode)) {
-            views_Fn[url_params.mode](auth, url_params)
-            .then(view => {
-                this.setState({
-                    view,
-                    params: url_params,
+        db.auth().onAuthStateChanged(() => {
+            const url_params = _.pick(parse(search), ['mode', 'oobCode', 'continueUrl']);
+            const views_Fn = {
+                resetPassword: Promise.resolve(this.STATES.RESET_PASSWORD),
+                verifyEmail: this.verifyEmail,
+            };
+            console.log('url_params.mode', url_params.mode);
+            if (_.has(views_Fn, url_params.mode)) {
+                return views_Fn[url_params.mode](db, url_params)
+                .then(view => {
+                    this.setState({
+                        view,
+                        params: url_params,
+                    });
                 });
-            });
-        } else {
+            }
             this.setState({
                 view: this.STATES.UNKNOWN,
                 params: url_params,
             });
-        }
+        });
     }
 
-    verifyEmail(auth, params) {
-        console.log('verifyEmail', params);
+    verifyEmail(db, params) {
+        //Fallback: Si ya estas validado mostramos la página buena.
+        if (_.get(db.auth().currentUser, 'emailVerified', false)) {
+            return Promise.resolve(this.STATES.EMAIL_VERIFIED);
+        }
+
         if (params.oobCode) {
-            return auth.applyActionCode(params.oobCode)
+            return db.auth().applyActionCode(params.oobCode)
             .then(() => this.STATES.EMAIL_VERIFIED)
             .catch(() => this.STATES.EMAIL_NOT_VERIFIED);
         }
         return Promise.resolve(this.STATES.UNKNOWN);
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        const propsEqual = _.isEqual(this.props, nextProps);
+        const stateEqual = _.isEqual(this.state, nextState);
+        return !propsEqual || !stateEqual;
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('shouldComponentUpdate', nextProps, nextState);
+    //     return true;
+    // }
     render() {
         const {
             t,
             className,
         } = this.props;
-        console.log(this.state.view);
+        console.log('render', this.state.view);
         return <PageContent className={className}>
+
             {
                 this.state.view === this.STATES.EMAIL_VERIFIED && <EmailVerified t={t}/>
             }
