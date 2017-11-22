21faec6351c9ef5f5a71055e29fc18d226628fe4
Miguel Caballero <mcaballero@tutellus.com>
2017-11-22 15:29:30 +0100

    copies mejorados

--- src/i18n/en/sections/platform.js
+++ src/i18n/en/sections/platform.js
@@ -1,5 +1,5 @@
 //@flow
 export const the_platform = {
-    title: "Live platform running since 2013",
+    title: "Full working platform since 2013",
     description: "We are the leading platform in 20 countries, and from now on we'll become the leading decentralized platform worlwide",
 };
