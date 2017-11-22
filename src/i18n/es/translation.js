e1584fd4f06756338d2a8e310fa6acb9c144404a
Miguel Caballero <mcaballero@tutellus.com>
2017-11-22 14:02:45 +0100

    copies

--- src/i18n/es/translation.js
+++ src/i18n/es/translation.js
@@ -4,7 +4,7 @@ export const translation = {
     transactions: "transacciones",
     learning: "aprendiendo",
     transactioned: "transaccionados",
-    about_tutellus: 'Tutellus es una Plataforma EdTech descentralizada basada en la Blockchain de Ethereum',
+    about_tutellus: 'Tutellus es la Plataforma educativa que te paga por aprender',
     terms_of_sale: 'Términos de la Token Sale',
     help_url: '//tutellusico.zendesk.com/hc/es',
     tos_url: '/pdf/tos_es.pdf',
