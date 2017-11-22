13fb93409103bb6bded6326f4f7be2662b2f3423
Miguel Caballero <mcaballero@tutellus.com>
2017-11-22 13:53:08 +0100

    leves cambios de copies

--- src/i18n/es/sections/the_solution.js
+++ src/i18n/es/sections/the_solution.js
@@ -1,5 +1,5 @@
 //@flow
 export const the_solution = {
-    title: "La solución: una plataforma que te paga por aprender",
+    title: "La solución: la plataforma que te paga por aprender",
     description: "Generamos nuestra propia criptomenda: el token TUT. Cuanto más estudias más dinero ganas y más relevante llegas a ser en la Comunidad, por lo que más interesante eres para ser contratado o contactado por empresas",
 };
