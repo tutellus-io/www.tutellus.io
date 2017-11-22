90c2fa79530763e683954f7d7f7d69ac214bd0a4
Miguel Caballero <mcaballero@tutellus.com>
2017-11-22 17:02:29 +0100

    mejorados copies

--- src/i18n/en/sections/team.js
+++ src/i18n/en/sections/team.js
@@ -1,7 +1,7 @@
 //@flow
 export const the_team = {
     title: "Team",
-    description: "A solid team growing together since 2014, summing up nearly 100 years of working experience and with next guys leading the project",
+    description: "A solid team growing together since 2014, summing up nearly 100 years of working experience and with the following crew",
     mcaballero: "Engineer & MBA. 20 years in tech industry",
     jortiz: "Fullstack Engineer, 20 years making DevTeams",
     clopez: "Engineer, Backend & Blockchain Developer",
