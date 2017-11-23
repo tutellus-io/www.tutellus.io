008a846693a74d5ea7d8185061787c78d9f5a4c9
Javier Ortiz <jortiz@tutellus.com>
2017-11-23 17:24:43 +0100

    Nuevas imagenes progresivas del Team

--- src/pages/home/sections/TheTeam.js
+++ src/pages/home/sections/TheTeam.js
@@ -12,32 +12,32 @@ export const TheTeam = translate('the_team')(({t, id}) =>
     <PageSection id={ id } title={ t('title') }>
         <Text center>{ t('description') }</Text>
         <Team>
-            <TeamMember name="Miguel Caballero" title="CEO" photo="//www.tutellus.com/bower_components/tutellus.css/images/team/Miguel.jpg" socialProfiles={ {
+            <TeamMember name="Miguel Caballero" title="CEO" photo="/images/team/Miguel.jpg" socialProfiles={ {
                 linkedin: '//www.linkedin.com/in/micaballero/',
                 twitter: '//twitter.com/mcaballero',
             } }>
                 { t('mcaballero') }
             </TeamMember>
-            <TeamMember name="Javier Ortiz" title="CTO" photo="//www.tutellus.com/bower_components/tutellus.css/images/team/JaviSok.jpg" socialProfiles={ {
+            <TeamMember name="Javier Ortiz" title="CTO" photo="/images/team/JaviSok.jpg" socialProfiles={ {
                 linkedin: '//www.linkedin.com/in/sokardys/',
                 github: '//github.com/sokardys',
                 twitter: '//twitter.com/sokardys',
             } }>
                 { t('jortiz') }
             </TeamMember>
-            <TeamMember name="Carlos López" title="Engineer" photo="//www.tutellus.com/bower_components/tutellus.css/images/team/Carlos.jpg" socialProfiles={ {
+            <TeamMember name="Carlos López" title="Engineer" photo="/images/team/Carlos.jpg" socialProfiles={ {
                 linkedin: '//www.linkedin.com/in/carlos-lópez-46314962/',
                 github: '//github.com/carlos-lopez',
                 twitter: '//twitter.com/clpz84',
             } }>
                 { t('clopez') }
             </TeamMember>
-            <TeamMember name="Javier Calvo" title="Data Scientist" photo="//www.tutellus.com/bower_components/tutellus.css/images/team/JaviMat.jpg" socialProfiles={ {
+            <TeamMember name="Javier Calvo" title="Data Scientist" photo="/images/team/JaviMat.jpg" socialProfiles={ {
                 linkedin: '//www.linkedin.com/in/jcalvo92/',
             } }>
                 { t('jcalvo') }
             </TeamMember>
-            <TeamMember name="Karol Szymanczak" title="Designer" photo="//www.tutellus.com/bower_components/tutellus.css/images/team/Karol.jpg" socialProfiles={ {
+            <TeamMember name="Karol Szymanczak" title="Designer" photo="/images/team/Karol.jpg" socialProfiles={ {
                 linkedin: '//www.linkedin.com/in/karolina-szymańczak-18206838/',
             } }>
                 { t('karol') }
