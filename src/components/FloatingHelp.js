aec375201f79c8c63da8d53d93f7d8f0418de571
Javier Ortiz <jortiz@tutellus.com>
2017-11-28 15:31:56 +0100

    Detalles varios de Estilos y Copys
    - Padding en PageSection y Footer
    - Underline H1, H2
    - Imágenes Home demasiado grandes

--- src/components/FloatingHelp.js
+++ src/components/FloatingHelp.js
@@ -4,12 +4,16 @@ import styled from 'styled-components';
 import {social_links} from '../config';
 import styles from '../styles';
 export const FloatingHelp = styled(props =>
-    <a className={ props.className } href={ social_links.telegram } />
+    <a className={ props.className }
+        href={ social_links.telegram }
+        target="_blank"
+    />
 )`
     position: fixed;
     z-index: 3;
     bottom: 0;
     right: 0;
+    cursor: pointer;
     &:before {
         content: '';
         background: url(${ props => props.icon }) center center no-repeat;
@@ -18,12 +22,12 @@ export const FloatingHelp = styled(props =>
         height: 3em;
         margin: 1em;
         @media ${ styles.media.tablet } {
-            width: 4em;
-            height: 4em;
+            width: 3em;
+            height: 3em;
         }
         @media ${ styles.media.desktop } {
-            width: 5em;
-            height: 5em;
+            width: 4em;
+            height: 4em;
         }
     }
 `;
