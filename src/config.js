faf4832c7397ac28873501d02ed6dc050145dace
Carlos López <clopez@tutellus.com>
2017-12-11 11:08:20 +0100

    feat(social-links): link to facebook page
    closes #20

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ export const social_links = {
     telegram: '//t.me/tutellus',
     medium: '//medium.com/tutellus-io',
     twitter: '//twitter.com/tutellusico',
+    facebook: '//www.facebook.com/tutellusio/',
 };
 export const S3 = 'https://lib.tutellus.com/ico';
