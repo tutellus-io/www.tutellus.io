aa764c3070a8667bc3e10be76cee022803047f3f
Carlos López <clopez@tutellus.com>
2018-02-20 12:12:43 +0100

    feat(Landing): refactor of common page stucture (MainHeader and Footer)
    Allows for other pages besides home to use the same structure easily.

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -2,29 +2,31 @@
 import * as React from 'react';
 /*:: import type {ComponentType} from 'react' */
 import {translate} from 'react-i18next';
+import {withRouter} from 'react-router-dom';
+
 import {
     TopHeader,
     TOP_HEADER_HEIGHT,
     MainMenu,
     SecondaryMenu,
-} from '../../components';
-import {withWindowScroll} from '../../hoc';
+} from './TopHeader';
+import {withWindowScroll} from '../hoc';
 
 /*::
 type MainHeaderProps = {
-    history: void,
     socialLinks: Object,
 }
 */
-export const MainHeader/*:ComponentType<MainHeaderProps>*/ = withWindowScroll(translate()((({scroll, i18n, history, socialLinks}) =>
+export const MainHeader/*:ComponentType<MainHeaderProps>*/ = withWindowScroll(withRouter(translate()((({scroll, i18n, history, socialLinks}) =>
     <TopHeader small={ scroll.y > TOP_HEADER_HEIGHT.SMALL }
                logo="/images/white-logo.svg"
                title="Tutellus.io"
                notify>
+        { history.location.pathname === '/' &&
         <MainMenu />
+        }
         <SecondaryMenu onLanguage={ lang => i18n.changeLanguage(lang) }
                        socialLinks={ socialLinks }
-                       locale={ i18n.language }
-                       history={ history } />
+                       locale={ i18n.language } />
     </TopHeader>
-/*:ComponentType<*>*/)));
+/*:ComponentType<*>*/))));
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,10 @@
 //@flow
 import React from 'react';
 /*:: import type {ComponentType} from 'react' */
-import R from 'ramda';
 import {injectGlobal} from 'styled-components';
 
 import styles from '../../styles';
 
-import {MainHeader} from './MainHeader';
 import {
     Advisors,
     ICOIntro,
@@ -25,51 +23,41 @@ import {
     SubscribeToMailList,
     Documents,
 } from './sections';
-import {MainFooter} from './MainFooter';
-import {NotifyBar} from '../../components';
-import {observer, inject} from 'mobx-react';
-
-const MAX_SOCIAL_LINKS_IN_HEADER = 3;
-const pickMostRelevant = social_links =>
-    R.pick(R.take(MAX_SOCIAL_LINKS_IN_HEADER, R.keys(social_links)), social_links);
+import {
+    AmbassadorsJoinSection,
+} from '../ambassadors';
+import {Landing} from '../../components';
 
 /*::
 type Props = {|
     config: {social_links: any},
-    history: any,
 |}
 */
-export const Home/*:ComponentType<Props>*/= inject('config')(observer(class extends React.Component/*::<Props>*/ {
+export const Home/*:ComponentType<Props>*/= class extends React.Component/*::<Props>*/ {
     componentDidMount() {
         injectGlobal`${ styles.global }`;
     }
     render() {
-        const social_links = this.props.config.social_links;
         return (
-            <div>
-                <MainHeader socialLinks={ pickMostRelevant(social_links) }
-                            history={ this.props.history } />
-                <NotifyBar />
-                <main>
-                    <ICOIntro />
-                    <ThePain />
-                    <TheSolution />
-                    <PlatformBenefits />
-                    <HowItWorks />
-                    <Documents id="documents" />
-                    <ThePlatform id="platform" />
-                    <PraiseQuotes />
-                    <TheTeam id="team" />
-                    <TheRoadshow/>
-                    <PlatformRoadmap />
-                    <Crowdsale id="crowdsale" />
-                    <TechnologyStack />
-                    <Advisors />
-                    <Partners />
-                    <SubscribeToMailList />
-                </main>
-                <MainFooter socialLinks={ social_links } />
-            </div>
+            <Landing>
+                <ICOIntro />
+                <ThePain />
+                <TheSolution />
+                <PlatformBenefits />
+                <HowItWorks />
+                <Documents id="documents" />
+                <ThePlatform id="platform" />
+                <PraiseQuotes />
+                <TheTeam id="team" />
+                <TheRoadshow/>
+                <AmbassadorsJoinSection link="/ambassadors" />
+                <PlatformRoadmap />
+                <Crowdsale id="crowdsale" />
+                <TechnologyStack />
+                <Advisors />
+                <Partners />
+                <SubscribeToMailList />
+            </Landing>
         );
     }
-}));
+};
