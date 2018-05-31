e738574b4300d071cd6abe6f9b9f39dc041b9c0d
Javier Ortiz <jortiz@tutellus.com>
2018-05-31 17:11:26 +0200

    feat(toast) Adding toast with the latest ICO purchases

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import {unregister} from './registerServiceWorker';
 import * as config from './config';
 
 import Master from './Master';
+import {ICOPurchases} from './components';
 
 ReactDOM.render(
     <Provider config={config}>
-        <Master />
+        <React.Fragment>
+            <Master />
+            <ICOPurchases />
+        </React.Fragment>
     </Provider>
 //$FlowFixMe #root existe
 , document.getElementById('root'));
