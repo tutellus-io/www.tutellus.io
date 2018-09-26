2a707b14cfacf857ead6f90a307e96a2d4e650a4
Javier Ortiz <jortiz@tutellus.com>
2018-09-26 12:24:14 +0200

    refactor(flow-types) updating flow-types

--- src/pages/home/sections/ICOIntro.js
+++ src/pages/home/sections/ICOIntro.js
@@ -2,7 +2,7 @@
 /*eslint no-magic-numbers: off*/
 import React from 'react';
 import PropTypes from 'prop-types';
-import {observer, inject} from 'mobx-react';
+import {observer} from 'mobx-react';
 import {translate} from 'react-i18next';
 import styled from 'styled-components';
 import {
@@ -14,34 +14,20 @@ import {
 
 import styles from '../../../styles';
 
-export const ICOIntro = translate('intro')(inject('config')(observer(styled(class extends React.Component {
-    constructor() {
-        super();
-        this.state = {};
-    }
-
-    render() {
-        const {
-            t,
-            className,
-        } = this.props;
-
-        return (
-            <PageBanner className={ className }>
-                <PageTitle margin={false}
-                    dangerouslySetInnerHTML={ {__html: t("title")} } />
-                <PlayButton video={ t('video_url') } />
-            </PageBanner>
-        );
-    }
-})`
+export const ICOIntro = translate('intro')(observer(styled(({className, t}) =>
+    <PageBanner className={ className }>
+        <PageTitle margin={false}
+            dangerouslySetInnerHTML={ {__html: t("title")} } />
+        <PlayButton video={ t('video_url') } />
+    </PageBanner>
+)`
     @media ${ styles.media.tablet } {
         & > ${ SectionContent } {
             grid-template-columns: 1fr;
             grid-column-gap: 1.5em;
         }
     }
-`)));
+`));
 
 ICOIntro.displayName = 'ICOIntro';
 ICOIntro.propTypes = {
